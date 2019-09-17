c86cb042c970db92daacce67493d0a9c77b22da5
Simisoluwa <Adeyemo1882@gmail.com>
2019-09-17 18:19:13 +0100

    Created the hero background and the menu items and made it responsive using css-grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,20 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import { Link } from "gatsby";
+import Header from '../components/header';
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <div>
+    <Header/>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
+   
+  </div>
 )
 
 export default IndexPage
